1e3022021a0d33949816040e0ad8c81e10108e18
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-08 12:03:55 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+
+function addingEventListener() {
+}
+
+function addingEventListener() {
+    // Create a reference to the input element
+    const input = document.getElementById('input');
+
+    // Create a named function for the event handler
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+
+    // Add a click event listener to the input element
+    input.addEventListener('click', clickAlert);
+  }
+
+  // Call the outer function to set up the event listener
+  addingEventListener();
 }
